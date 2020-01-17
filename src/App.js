f67c87238885c408e229ef2c595ef60c52b09a43
cpangelinan <43481392+cpangelinan@users.noreply.github.com>
2020-01-17 09:04:58 -0800

    initial push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <main>
+
+    <header> 
+      <img className="logo"
+        alt="logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Pac_Man.svg/1200px-Pac_Man.svg.png"
+      />
+      Chatbox
+    </header>
+
+    <TextInput onSend={t=> console.log(t)}/>
+
+  </main>
+
+  function TextInput(props){
+    const [text,setText] = useState('')
+    
+    return <div className="text-input">
+    <input className="text"
+      value={text}
+      placeholder="sup, hoe?"
+      onChange={e => setText(e.target.value)}
+    />
+    
+    <button className="button"
+      onClick={()=> {
+      props.onSend(text)
+      setText('')}}>
+      SEND
+    </button>
+
     </div>
-  );
+  }
 }
+
 
 export default App;
